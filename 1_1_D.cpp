90e1099605e5fd61d67543d6f05b6a45641b7fcd
o-xian999 <onoyama1999910@gmail.com>
2020-02-22 22:07:45 +0900

    Edit 1_1_D

--- 1_1_D.cpp
+++ 1_1_D.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main (){
   int S, S_1, S_2, S_3;
   cin >> S;
-  S_1 = S / 24 / 60 / 60;
-  S_2 = S / 24 / 60;
-  S_3 = S / 24;
+  S_1 = S / (60 * 60);
+  S_2 = (S - S_1 * 3600) / 60;
+  S_3 = S - S_1 * 3600 - S_2 * 60;
 
   cout << S_1 << ":" << S_2 << ":" << S_3 << endl;
 }
