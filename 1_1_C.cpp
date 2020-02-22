be2a3e5228a4e0c33d76152e8718ac59495266ce
o-xian999 <onoyama1999910@gmail.com>
2020-02-22 21:43:56 +0900

    Solve 1_1_C

---
+++ 1_1_C.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int a,b;
+  cin >> a >> b;
+  //Separate first value and sedond value by using space.
+  cout << a * b << " " <<  a + a + b + b << endl;
+}
