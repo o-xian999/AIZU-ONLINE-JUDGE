8720d1d2d18818e555c5c839aae4684a3e225d52
o-xian999 <onoyama1999910@gmail.com>
2020-02-23 15:15:25 +0900

    Solve 1_3_C

---
+++ 1_3_C.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <utility>
+ using namespace std;
+
+int main (){
+  //int x, y;
+  //cin >> x >> y;
+  //if(x > y) swap(x, y);
+  //cout << x << " " << y << endl;
+  
+  int x, y;
+  for(int i = 1; i < 10001; i++){
+    cin >> x >> y;
+    if((x == 0)&&(y == 0)) break;
+    else if(x > y) swap(x,y);
+    cout << x << " " << y << endl;
+    }
+}
+
