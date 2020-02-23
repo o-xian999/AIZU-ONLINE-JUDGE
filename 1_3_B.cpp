6dbf6fa219080da06a297511a1ed025005ae56a4
o-xian999 <onoyama1999910@gmail.com>
2020-02-23 14:56:00 +0900

    Solve 1_3_B

--- 1_3_B.cpp
+++ 1_3_B.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 
-using namespace std:
+using namespace std;
 
 int main (){
   int x;
 
-  for(int i = 0; i < )
+  for(int i = 1; i <= 10000; i++){
+    cin >> x;
+
+    if(x == 0) break;
+    else{
+      cout << "Case " << i << ": " << x << endl;
+    }
+
+    //cout << "Case " << i << ": " << x << endl;
+    //if(x == 0) break;
+    
+  }
 }
