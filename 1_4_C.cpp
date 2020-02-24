7f8006dd30a4883faf78f3a357a7e844b878c24a
o-xian999 <onoyama1999910@gmail.com>
2020-02-24 20:20:49 +0900

    Solve 1_4_C

---
+++ 1_4_C.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main (){
+  int a,c;
+  string b;
+  for(int i = 0; i < 20000; i++){
+    cin >> a >> b >> c;
+
+    if(b == "+") cout << a + c << endl;
+    else if(b == "-") cout << a - c << endl;
+    else if(b == "*") cout << a * c << endl;
+    //else if(b == "/") && (c == 0) Nothing done
+    else if((b == "/") && (c != 0)) cout << a / c << endl;
+    else if(b == "?") break;
+  }
+}
