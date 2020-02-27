24e7601fe28a389d201a691d80a9995f04b1b57b
o-xian999 <onoyama1999910@gmail.com>
2020-02-27 14:23:54 +0900

    Solve 1_5_C

---
+++ 1_5_C.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  while(true){
+    int a, b;
+    if(a == 0 && b == 0) break;
+    cin >> a >> b;
+    for(int i = 0; i < a; i++){
+      for(int j = 0; j < b; j++){
+        if((i + j) % 2 == 0) cout << "#";
+        else cout << ".";
+      }
+      cout << endl;
+    }
+    cout << endl;
+  }
+}
