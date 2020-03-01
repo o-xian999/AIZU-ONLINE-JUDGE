28bf82197b4465e51b48c23db42f42d3416c0589
o-xian999 <onoyama1999910@gmail.com>
2020-03-01 23:29:43 +0900

    Write 1_6_D

---
+++ 1_6_D.cpp
@@ -0,0 +1,52 @@
+//#include <iostream>
+//#include <vector>
+//
+//using namespace std;
+//
+//int main (){
+//  int n, m;
+//  cin >> n >> m;
+//  //vector<vector<int>> a(n, vector<int>(m));
+//  int a[101][101], b[100];
+//
+//  //cout << a[2] << endl;
+//
+//  for(int i = 0; i < n; i++){
+//    for(int j = 0; j < m; j++){
+//      a[i][j] = 0;
+//      cin >> a[i][j];
+//    }
+//  }
+//
+//  //cout << a[2][1] << endl;
+//
+//  //vector<vector<int>> b(m, vector<int>(1))
+//  //int b[100];
+//
+//  for(int j = 0; j < m; j++){
+//    //cin >> b[m][1];
+//    b[j] = 0;
+//    cin >> b[j];
+//  }
+//  cout << b[1] << endl;
+//
+//  //vector<vector<int>> c(n, vector<int>(1));
+//  //int c[n] = {0};
+//
+//  for(int i = 0; i < n; i++){
+//    //c[1] += a[1][i] * b[i][1];
+//    int c = 0;
+//    for(int j = 0; j < m; i++){
+//      c += a[i][j] * b[j];
+//      //cout << c[i] << endl;
+//    }
+//    cout << c << endl;
+//  }
+//
+//  return 0;
+//
+////  for(int i = 0; i < n; i++){
+////    cout << c[i] << endl;
+////  }
+//
+//}
