fac83c1177b0804a09e10ca7e2dcba75f5a09454
o-xian999 <onoyama1999910@gmail.com>
2020-03-02 22:28:53 +0900

    Cannot understand what's wrong

--- 1_6_D.cpp
+++ 1_6_D.cpp
@@ -50,3 +50,51 @@
 ////  }
 //
 //}
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int a[100][100];
+  int b[100];
+  int n, m, c = 0;
+
+  cin >> n >> m;
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < m; j++){
+      cin >> a[i][j];
+    }
+  }
+  for(int i = 0; i < m; i++)cin >> b[i];
+  
+  for(int i = 0; i < n; n++){
+    for(int j = 0; i < m; m++){
+      c += a[i][j]*b[j];
+    }
+    cout << c << endl;
+    c = 0;
+  }
+}
+
+#include<iostream>
+using namespace std;
+int main(){
+  int n,m;
+  int i,j,k;
+  int mat[100][100]={{}};
+  int ve[100]={};
+  cin >> n >> m;
+  for(i=0;i<n;i++){
+    for(j=0;j<m;j++){
+      cin >> mat[i][j];
+    }
+  }
+  for(j=0;j<m;j++) cin >> ve[j];
+  for(i=0;i<n;i++){
+    int x=0;
+    for(j=0;j<m;j++) x+=mat[i][j]*ve[j];
+    cout << x << endl;
+  }
+
+  return 0;
+}
